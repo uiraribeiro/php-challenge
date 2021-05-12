ea6ddb8341dc5c69bc009df2e3780362199fe36e
Vanderson Mota <v.mota@syseleven.de>
2021-05-12 09:11:36 +0200

    list controllers

--- src/AppBundle/Controller/ProductsController.php
+++ src/AppBundle/Controller/ProductsController.php
@@ -3,16 +3,20 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\Products\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ProductsController
+class ProductsController extends Controller
 {
     /**
      * @Route("/products")
      */
     public function listProducts()
     {
-        return new JsonResponse(
-            array(array('name' => 'Cloud on demand'))
-        );
+        $products = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->findAll();
+        $products = array_map(function($p) {return array('name' => $p.getName, 'price' => $p.getPrice(), 'description' => $p.getDescription());}, $products);
+        return new JsonResponse($products);
     }
 }
